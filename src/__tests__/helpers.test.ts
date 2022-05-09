faa2c7d4dc27fc731dfaf1dfd5bce04a39572a79
Mohammad Quadri <quadrixm@gmail.com>
2022-05-09 13:27:43 +0530

    adding more functions 🎉🎉🎉

--- src/__tests__/helpers.test.ts
+++ src/__tests__/helpers.test.ts
@@ -1,5 +1,70 @@
-import { toSubstring } from '../index';
+import { equalNum, isPromise, parseNum, reduceUniqueIds, sortByDate, sortById, toSubstring } from '../index';
 
-test('toSubstring', () => {
-  expect(toSubstring('Hello Carl', 6, '...')).toBe('Hello...');
+describe('helpers fucntions', () => {
+  // Applies only to tests in this describe block
+  beforeEach(() => {
+    // return initializeFoodDatabase();
+  });
+
+  test('toSubstring', () => {
+    expect(toSubstring('Hello Carl', 6, '...')).toBe('Hello...');
+  });
+  
+  test('isPromise', () => {
+    const promiseFn = async () => { return Promise.resolve(() => console.log('Hello world!')) }
+    const nonPromiseFn = () => { console.log('Hello world!')}
+    expect(isPromise(promiseFn())).toBe(true);
+    expect(isPromise(nonPromiseFn())).toBe(false);
+  });
+  
+  test('parseNum', () => {
+    expect(parseNum('1')).toBe(1);
+    expect(parseNum(1)).toBe(1);
+    expect(parseNum('one')).toBe(0);
+    expect(parseNum(undefined)).toBe(0);
+    expect(parseNum(null)).toBe(0);
+  });
+  
+  test('equalNum', () => {
+    expect(equalNum('1', 1)).toBe(true);
+    expect(equalNum(1, 1)).toBe(true);
+    expect(equalNum(1, undefined)).toBe(false);
+    expect(equalNum(0, undefined)).toBe(false);
+    expect(equalNum(1, null)).toBe(false);
+    expect(equalNum(0, null)).toBe(false);
+    expect(equalNum(0, 0)).toBe(false);
+    expect(equalNum('0', 0)).toBe(false);
+  });
+  
+  test('reduceUniqueIds', () => {
+    const randomIds = [23, 43, 11, 11, 22, 22];
+    const randomObjs = [{key: "abc"}, {key: "abc"}, {key: "def"}, {key: "abc"}];
+    const ids = randomIds.reduce<number[]>(
+      (ids, id) => reduceUniqueIds(ids, id),
+      [],
+    );
+    const keys = randomObjs.reduce<string[]>(
+      (keys, {key}) => reduceUniqueIds(keys, key),
+      [],
+    );
+    expect(ids).toEqual([23, 43, 11, 22]);
+    expect(keys).toEqual(["abc", "def"]);
+  });
+  
+  test('sortById', () => {
+    const randomObjs = [{id: 12, value: "abc"}, {id: 2, value: "def"}, {id: 22, value: "ghi"}];
+    randomObjs.sort(({id: id1}, {id: id2}) => sortById(id1, id2));
+    expect(randomObjs).toEqual([{id: 2, value: "def"}, {id: 12, value: "abc"}, {id: 22, value: "ghi"}]);
+    randomObjs.sort(({id: id1}, {id: id2}) => sortById(id1, id2, true));
+    expect(randomObjs).toEqual([{id: 22, value: "ghi"}, {id: 12, value: "abc"}, {id: 2, value: "def"}]);
+  });
+  
+  test('sortByDate', () => {
+    const randomObjs = [{id: 12, date: "5-15-2022"}, {id: 12, date: "5-12-2022"}, {id: 12, date: "5-1-2022"}];
+    randomObjs.sort(({date: d1}, {date: d2}) => sortByDate(d1, d2));
+    expect(randomObjs).toEqual([{id: 12, date: "5-1-2022"}, {id: 12, date: "5-12-2022"}, {id: 12, date: "5-15-2022"}]);
+    randomObjs.sort(({date: d1}, {date: d2}) => sortByDate(d1, d2, true));
+    expect(randomObjs).toEqual([{id: 12, date: "5-15-2022"}, {id: 12, date: "5-12-2022"}, {id: 12, date: "5-1-2022"}]);
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,47 @@
 export const toSubstring = (str?: string, length = 0, endsWith = '') =>
   str && str?.length >= length ? str.slice(0, length - 1) + endsWith : str;
 
-export const isPromise = (foo: any) => foo.then && typeof foo.then === 'function';
+export const isPromise = (foo: any) => !!foo?.then && typeof foo.then === 'function';
 
 export const parseNum = (num: number | string | undefined | null) =>
-  num ? parseInt(String(num), 10) : 0;
+  parseInt(String(num), 10) || 0;
 
 export const equalNum = (
-  num1: number | string | undefined,
-  num2: number | string | undefined,
-) => num1 && num2 && parseNum(num1) === parseNum(num2);
+  num1: number | string | undefined | null,
+  num2: number | string | undefined | null,
+) => !!num1 && !!num2 && parseNum(num1) === parseNum(num2);
 
-export const reduceUniqueIds = (ids: number[], id?: number) => {
-  if (id && !ids.includes(id)) {
-    ids.push(id);
-  }
-  return ids;
-};
+export function reduceUniqueIds<T>(ids: T[], id?: T) {
+	if (id && !ids.includes(id)) {
+		ids.push(id);
+	}
+	return ids;
+}
 
-export const sortById = (id1?: number, id2?: number): number => {
+export const sortById = (id1?: number, id2?: number, desc = false): number => {
   if (!id1 ) {
     return 1;
   }
   if (!id2 ) {
     return -1;
   }
-  return id2 != id1 ? (id2 - id1 > 0 ? 1 : -1) : 0;
+  if (desc) {
+    return id2 != id1 ? (id2 - id1 > 0 ? 1 : -1) : 0;
+  } else {
+    return id2 != id1 ? (id1 - id2 > 0 ? 1 : -1) : 0;
+  }
 };
 
-export const sortByDate = (d1?: string, d2?: string): number => {
+export const sortByDate = (d1?: string, d2?: string, desc = false): number => {
   if (!d1 ) {
     return 1;
   }
   if (!d2 ) {
     return -1;
   }
-  return d2 != d1 ? (new Date(d2).getTime() - new Date(d1).getTime() > 0 ? 1 : -1) : 0;
+  if (desc) {
+    return d2 != d1 ? (new Date(d2).getTime() - new Date(d1).getTime() > 0 ? 1 : -1) : 0;
+  } else {
+    return d2 != d1 ? (new Date(d1).getTime() - new Date(d2).getTime() > 0 ? 1 : -1) : 0;
+  }
 };
